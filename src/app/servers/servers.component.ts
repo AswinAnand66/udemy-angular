cd91a244e335d386889729e7aae86665ca8b6288
AswinAnand66 <aswinananad66@gmail.com>
2022-06-23 14:22:04 +0530

    Assignment 2 completed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
