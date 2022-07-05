4402a6b07f4583a4a1086a5d24cb542a10324505
AswinAnand66 <aswinananad66@gmail.com>
2022-07-05 18:19:56 +0530

    49 creating the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning.alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { AssignmentComponent } from './assignment/assignment.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +24,13 @@ import { AssignmentComponent } from './assignment/assignment.component';
     WarningAlertComponent,
     SuccessAlertComponent,
     AssignmentComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
