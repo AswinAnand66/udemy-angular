72eccc6401e2f29de46fc524074ed91988e1d5be
AswinAnand66 <aswinananad66@gmail.com>
2022-07-07 12:12:39 +0530

    Adding content to recipe component

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipie.model';
 
 @Component({
   selector: 'app-recipie-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
-
+  recipies: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://www.licious.in/blog/wp-content/uploads/2020/10/butter-chicken-.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
